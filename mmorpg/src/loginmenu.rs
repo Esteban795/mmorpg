c7d19ec649ca839199a7bf03e131b476fdb49819
MANDJUKOWSKI Esteban <estebanmandjukowski@gmail.com>
2026-05-11 19:17:30 -0400

    Feat: [+] AppState [+] LoginMenu with bevy_eguy (pseudo, connect, quit buttons)

---
+++ mmorpg/src/loginmenu.rs
@@ -0,0 +1,66 @@
+use bevy::app::AppExit;
+use bevy::prelude::*;
+use bevy_egui::{EguiContexts, EguiPrimaryContextPass, egui};
+
+
+use crate::state::AppState;
+
+#[derive(Resource, Default)]
+pub struct ConnectionSettings {
+    pub username: String,
+}
+
+pub struct LoginMenulugin;
+
+impl Plugin for LoginMenulugin {
+    fn build(&self, app: &mut App) {
+        // Run iff in LoginMenu state
+        app.init_resource::<ConnectionSettings>()
+            .add_systems(Startup, setup_camera.run_if(in_state(AppState::LoginMenu)))
+            .add_systems(
+                EguiPrimaryContextPass,
+                menu_ui.run_if(in_state(AppState::LoginMenu)),
+            );
+    }
+}
+
+fn setup_camera(mut commands: Commands) {
+    commands.spawn(Camera2d);
+}
+
+fn menu_ui(
+    mut contexts: EguiContexts,
+    mut settings: ResMut<ConnectionSettings>,
+    mut next_state: ResMut<NextState<AppState>>,
+    mut exit: MessageWriter<AppExit>,
+) {
+    let Ok(ctx) = contexts.ctx_mut() else {
+        return;
+    };
+
+    egui::Window::new("Connexion au Serveur")
+        .anchor(egui::Align2::CENTER_CENTER, egui::vec2(0.0, 0.0))
+        .collapsible(false)
+        .resizable(false)
+        .show(ctx, |ui| {
+            ui.vertical_centered(|ui| {
+                ui.label("Nom d'utilisateur :");
+
+                ui.add(egui::TextEdit::singleline(&mut settings.username).hint_text("Pseudo"));
+
+                ui.add_space(10.0);
+
+                if ui.button("Se Connecter").clicked() {
+                    if !settings.username.is_empty() {
+                        // CHANGE THIS TO ACTUALLY CONNECT TO THE SERVER
+                        println!("Connexion de {}", settings.username);
+                        next_state.set(AppState::InGame);
+                    }
+                }
+
+                if ui.button("Quitter").clicked() {
+                    exit.write(AppExit::Success);
+                }
+            });
+        });
+}
--- mmorpg/src/main.rs
+++ mmorpg/src/main.rs
@@ -1,3 +1,18 @@
+mod loginmenu;
+mod state;
+
+use bevy::prelude::*;
+use bevy_egui::EguiPlugin;
+use loginmenu::LoginMenulugin;
+use state::AppState;
+
 fn main() {
     println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(LoginMenulugin)
+        .add_plugins(EguiPlugin::default())
+        .init_state::<AppState>()
+        .insert_state(AppState::LoginMenu)
+        .run();
 }
