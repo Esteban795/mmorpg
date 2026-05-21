05b2bbcc64a78601d5621869b32291f4307f99a7
MANDJUKOWSKI Esteban <estebanmandjukowski@gmail.com>
2026-05-21 00:18:51 -0400

    fix: prevent log flooding by commenting the log called when moving

--- mmorpg_lab/client/src/game.rs
+++ mmorpg_lab/client/src/game.rs
@@ -57,9 +57,9 @@ fn player_input(keyboard: Res<ButtonInput<KeyCode>>, mut client: ResMut<QuinnetC
         x += 1.0;
     }
 
-    if (x, y) != (0.0, 0.0) { 
-        info!("Player input: x={}, y={}", x, y);
-    }
+    // if (x, y) != (0.0, 0.0) { 
+    //     info!("Player input: x={}, y={}", x, y);
+    // }
     if x != 0.0 || y != 0.0 {
         let connection = client.connection_mut();
         let _ = connection.send_message(ClientMessage::MoveInput { x, y });
