bbfd20d3114a5bfe2016749e089ee9b3edabd1ec
MANDJUKOWSKI Esteban <estebanmandjukowski@gmail.com>
2026-06-02 17:06:09 -0400

    feat: fix to fakebroker to avoid spamming spatial server

--- mmorpg_lab/fakebroker/src/main.rs
+++ mmorpg_lab/fakebroker/src/main.rs
@@ -3,9 +3,9 @@ use game_sockets::{
     GameConnection, GameNetworkEvent, GamePeer, GameStream, GameStreamReliability,
     protocols::QuicBackend,
 };
-use tracing::{Level, error, info};
+use shared::broker_protocol::{BrokerMessage, topic_to_string};
+use tracing::{Level, error, info, warn};
 use tracing_subscriber::FmtSubscriber;
-use shared::broker_protocol::BrokerMessage;
 fn main() {
     let subscriber = FmtSubscriber::builder()
         .with_max_level(Level::INFO)
@@ -23,49 +23,54 @@ fn main() {
 
     let mut reliable_stream: Option<GameStream> = None;
     let mut unreliable_stream: Option<GameStream> = None;
-    let mut conn: Option<GameConnection> = None;
+    let mut conn : Option<GameConnection> =  None;
+
+    let mut x = 12.0;
+    let mut y = 12.0;
     loop {
         while let Ok(Some(event)) = peer.poll() {
             match event {
                 GameNetworkEvent::Connected(connection) => {
                     info!("[NETWORK] Client connected: {:?}", connection.connection_id);
+
                     conn = Some(connection);
-                    peer.create_stream(connection, GameStreamReliability::Reliable);
-                    peer.create_stream(connection, GameStreamReliability::Unreliable);
+                    if let Ok(_) = peer.create_stream(connection, GameStreamReliability::Reliable) {
+                        info!(
+                            "Reliable stream created for client {}",
+                            connection.connection_id
+                        );
+                    } else {
+                        error!(
+                            "Failed to create reliable stream for client {}",
+                            connection.connection_id
+                        );
+                    }
                 }
                 GameNetworkEvent::StreamCreated(connection, stream) => {
                     info!(
-                        "[NETWORK] Stream created for client {}: reliable={}",
+                        "[NETWORK] Stream created for client {}: reliable={},stream_id={}",
                         connection.connection_id,
-                        stream.is_reliable()
+                        stream.is_reliable(),
+                        stream.stream_id
                     );
                     match stream.is_reliable() {
                         true => {
                             reliable_stream = Some(stream);
-                            if let Some(rel_stream) = reliable_stream {
-                                let msg = BrokerMessage::PositionUpdate { client_id: 12, x: 12.0, y: 12.0 };
-                                if let Err(e) = peer.send(
-                                    &connection,
-                                    &rel_stream,
-                                    Bytes::from(msg.to_bytes()))
+                            if let Some(ref rel_stream) = reliable_stream {
+                                let msg = BrokerMessage::PositionUpdate {
+                                    client_id: 12,
+                                    x: x,
+                                    y: y,
+                                };
+                                if let Err(e) =
+                                    peer.send(&connection, &rel_stream, Bytes::from(msg.to_bytes()))
                                 {
                                     error!("Failed to send message on reliable stream: {:?}", e);
                                 }
                             }
                         }
                         false => {
-                            unreliable_stream = Some(stream);
-                            if let Some(unrel_stream) = unreliable_stream {
 
-                                let msg = BrokerMessage::PositionUpdate { client_id: 12, x: 12.0, y: 12.0 };
-                                if let Err(e) = peer.send(
-                                    &connection,
-                                    &unrel_stream,
-                                    Bytes::from(msg.to_bytes()))
-                                {
-                                    error!("Failed to send message on unreliable stream: {:?}", e);
-                                }
-                            }
                         }
                     }
                 }
@@ -84,7 +89,7 @@ fn main() {
                 }
                 GameNetworkEvent::Message {
                     connection,
-                    stream: _,
+                    stream,
                     data,
                 } => {
                     info!(
@@ -92,10 +97,55 @@ fn main() {
                         connection.connection_id,
                         data.len()
                     );
-                    // handle_client_message(&connection, &data, &mut registry, &mut net);
+                    if let Some(message) = BrokerMessage::from_bytes(&data) {
+                        match message {
+                            BrokerMessage::Subscribe { client_id, topic } => {
+                                let topic_str = topic_to_string(&topic);
+                                info!(
+                                    "Received Subscribe from client {}: topic={}",
+                                    client_id, topic_str
+                                );
+
+                                x += 10.0;
+                                y += 10.0;
+
+                                let update_msg = BrokerMessage::PositionUpdate { client_id, x, y };
+
+                                info!(
+                                    "Sending PositionUpdate to client {}: x={}, y={} on connection {:?}",
+                                    client_id, x, y, connection
+                                );
+                                if let Some(ref unrel_stream) = unreliable_stream {
+                                    if let Err(e) = peer.send(
+                                        &connection,
+                                        &unrel_stream,
+                                        Bytes::from(update_msg.to_bytes()),
+                                    ) {
+                                        error!(
+                                            "Failed to send message on unreliable stream: {:?}",
+                                            e
+                                        );
+                                    }
+                                }
+                            }
+                            _ => {
+                                warn!(
+                                    "Received unsupported message type from client: {:?}",
+                                    message
+                                );
+                            }
+                        }
+                    } else {
+                        warn!(
+                            "{} {}",
+                            connection.connection_id.to_string(),
+                            "Received invalid message format from client"
+                        );
+                    }
                 }
                 _ => {}
             }
         }
+        std::thread::sleep(std::time::Duration::from_millis(16));
     }
 }
