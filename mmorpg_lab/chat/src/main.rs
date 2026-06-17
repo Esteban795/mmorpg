e96d4c91ef9a2d55282c9b1f7268bfaa754673f2
MANDJUKOWSKI Esteban <estebanmandjukowski@gmail.com>
2026-06-17 01:22:16 -0400

    fix: change hardcoded string for badwords.txt + changed location

--- mmorpg_lab/chat/src/main.rs
+++ mmorpg_lab/chat/src/main.rs
@@ -1,6 +1,8 @@
 pub mod chat_service;
 pub mod moderator;
 
+use std::path::PathBuf;
+
 use game_sockets::{GamePeer, protocols::QuicBackend};
 use shared::{DEFAULT_BROKER_IP, DEFAULT_BROKER_PORT};
 use tracing::{Level, error, info};
@@ -31,7 +33,15 @@ fn main() {
         .parse()
         .expect("Invalid BROKER_PORT");
 
-    let moderator = Moderator::new("/home/esteban/CODE/mmorpg/mmorpg_lab/chat/src/badwords.txt");
+    // Find the absolute file path of the badwords.txt file
+    let manifest_dir = env!("CARGO_MANIFEST_DIR");
+    let mut badwords_path = PathBuf::from(manifest_dir);
+    badwords_path.push("assets");
+    badwords_path.push("badwords.txt");
+    let path_str = badwords_path.to_str().expect("Chemin invalide");
+
+    info!("Loading bad words from file: {}", path_str);
+    let moderator = Moderator::new(path_str);
 
     if let Err(e) = peer.connect(&broker_addr, broker_port) {
         error!("Failed to connect to broker: {:?}", e);
